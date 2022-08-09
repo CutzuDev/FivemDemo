dbba6191a167b5fda0a9f90992b51e787cc77ddc
CutzuDev <alexfarkas05@gmail.com>
2022-08-09 22:34:20 +0300

    Last Pushes for tonight

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const blurtimeout = setTimeout(blurAdd, 2150);
+const blurtimeout = setTimeout(blurAdd, 2100);
 
 function blurAdd() {
   document.querySelector(".header__right__half").style.backdropFilter =
