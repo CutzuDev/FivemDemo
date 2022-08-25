9a11da6e557cb4174d741528e453b1f08bad8bae
CutzuDev <alexfarkas05@gmail.com>
2022-08-25 22:26:46 +0300

    New anims and design

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
-const blurtimeout = setTimeout(blurAdd, 2100);
+const twoPointOneSecTimeout = setTimeout(blurAdd1, 2100);
+const onePointOneSecTimeout = setTimeout(blurAdd2, 1100);
+const twoSecTimeout = setTimeout(headerShadow, 2100);
 
-function blurAdd() {
+function blurAdd1() {
   document.querySelector(".header__right__half").style.backdropFilter =
     "blur(3px)";
+}
+
+function blurAdd2() {
+  document.querySelector("nav").style.backdropFilter = "blur(5px)";
+}
+
+function headerShadow() {
+  document.querySelector(".header").style.boxShadow =
+    "0px 0px 20px rgba(0, 1, 0, 0.75)";
 }
